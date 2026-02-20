7d1b7b906753469013105efc44a8f7c514035573
SokLeangSiv <lsok@paragoniu.edu.kh>
2026-02-20 19:56:07 +0700

    first commit

---
+++ src/main/java/com/example/testing/bro/TestingBroApplication.java
@@ -0,0 +1,13 @@
+package com.example.testing.bro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingBroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingBroApplication.class, args);
+	}
+
+}
